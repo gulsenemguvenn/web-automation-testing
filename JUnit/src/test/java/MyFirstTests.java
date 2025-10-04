443187c203fc348fb93f181c37e4ab8c69c67c57
gulsenemguvenn <gulsenemguvenceng@gmail.com>
2025-10-04 15:37:53 +0300

    junit tests

--- JUnit/src/test/java/MyFirstTests.java
+++ JUnit/src/test/java/MyFirstTests.java
@@ -1,2 +1,37 @@
+import org.junit.*;
+
 public class MyFirstTests {
+
+    @BeforeClass
+    public static void beforeMyClass() {
+        System.out.println("Before Tests");
+    }
+
+    @Before
+    public void beforeMyTest() {
+        System.out.println("Before Test");
+    }
+
+    @Test
+    public void myTest1() {
+        System.out.println("Test 1");
+    }
+
+    @Test
+    public void myTest2() {
+        System.out.println("Test 2");
+    }
+
+    @After
+    public void afterMyTest() {
+        System.out.println("After Test");
+    }
+
+    @AfterClass
+    public static void afterMyClass() {
+        System.out.println("After Tests");
+    }
+
+
+
 }
